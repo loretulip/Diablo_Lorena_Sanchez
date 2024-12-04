52b0351b9014f67d4801c247691f847105fa4a74
loretulip <lorenasvez@gmail.com>
2024-12-04 21:28:51 +0100

    Lookat

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,18 +1,47 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
+    private Outline outline;
+    [SerializeField] private Texture2D cursorInteraccion;
+    [SerializeField] private Texture2D cursorPorDefecto;
+
+    [SerializeField] private float tiempoRotacion;
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        outline = GetComponent<Outline>();
+
+    }
+
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorInteraccion, Vector2.zero, CursorMode.Auto);
+        outline.enabled = true;
+    }
+
+    private void OnMouseExit()
+    {
+        Cursor.SetCursor(cursorPorDefecto, Vector2.zero, CursorMode.Auto);
+        outline.enabled = false;
+    }
+
+    public void Interactuar(Transform interactuador)
+    {
+        Debug.Log("Holaaaaaaaaaaaaa");
+        transform.DOLookAt(interactuador.position, tiempoRotacion, AxisConstraint.X, Vector3.up);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,13 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float distanciaInteraccion;
+
     private NavMeshAgent agent;
     private Camera cam;
+
+    // Guardo la info del NPC actual con el que voy a hablar
+    private NPC npcActual;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +23,35 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Movimiento();
+        if(npcActual)
+        {
+            if (agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+            { 
+                Debug.Log("Holi");
+                npcActual.Interactuar(this.transform);
+                npcActual = null;
+                agent.isStopped = true;
+                agent.stoppingDistance = 0;
+            }
+        }
+    }
+
+    private void Movimiento()
+    {
         // Trazar un raycast desde la cámara a la posición del ratón
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray,out RaycastHit hit));
+        if (Physics.Raycast(ray, out RaycastHit hit)) ;
         {
-            if(Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0))
             {
+                // Mirar si el punto donde he impactado tiene el script NPC
+                if (hit.transform.TryGetComponent(out NPC npc))
+                {
+                    // Y en ese caso ese NPC es el actual
+                    npcActual = npc;
+                    agent.stoppingDistance = distanciaInteraccion;
+                }
                 agent.SetDestination(hit.point);
             }
         }
